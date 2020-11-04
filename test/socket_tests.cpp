19d3bfdbc48037beaad3ca8e2d19fb987708a480
Tobias Loew <tobias-loew@users.noreply.github.com>
2020-11-04 16:25:21 +0100

    Update socket_tests.cpp

--- test/socket_tests.cpp
+++ test/socket_tests.cpp
@@ -206,7 +206,7 @@ TEST_CASE("send/recv TCP/IPv4")
 		}()));
 }
 
-#if !CPPCORO_COMPILER_MSVC || CPPCORO_COMPILER_MSVC > 192000000 || !CPPCORO_CPU_X86
+#if !CPPCORO_COMPILER_MSVC || CPPCORO_COMPILER_MSVC >= 192000000 || !CPPCORO_CPU_X86
 // HACK: Don't compile this function under MSVC x86.
 // It results in an ICE under VS 2017.15 and earlier.
 
@@ -245,7 +245,7 @@ TEST_CASE("send/recv TCP/IPv4 many connections")
 		co_await s.disconnect();
 	};
 
-	auto echoServer = [&](cancellation_token ct) -> task<void>
+	auto echoServer = [&](cancellation_token ct) -> task<>
 	{
 		async_scope connectionScope;
 
@@ -304,7 +304,7 @@ TEST_CASE("send/recv TCP/IPv4 many connections")
 			CHECK(totalBytesReceived == 1000);
 		};
 
-		auto send = [&]() -> task<void>
+		auto send = [&]() -> task<>
 		{
 			std::uint8_t buffer[100];
 			for (std::uint64_t i = 0; i < 1000; i += sizeof(buffer))
